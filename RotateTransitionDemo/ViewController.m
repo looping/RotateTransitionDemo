64d0b70de73803def4df3405e87c31c72a2b29a7
Looping <www.looping@gmail.com>
2014-09-24 14:35:33 +0800

    Add spring animation

--- RotateTransitionDemo/ViewController.m
+++ RotateTransitionDemo/ViewController.m
@@ -10,12 +10,18 @@
 #import "FullscreenImageViewController.h"
 #import <POP.h>
 
+typedef NS_ENUM(NSUInteger, AnimationType) {
+    AnimationTypeBasic = 0,
+    AnimationTypeSpring,
+};
+
 @interface ViewController ()
 
 @property (nonatomic) UIImageView *imageView;
 @property (nonatomic) FullscreenImageViewController *imageViewController;
 @property (nonatomic) CGRect bakFrame;
 @property (nonatomic) BOOL presenting;
+@property (nonatomic) AnimationType animationType;
 
 @end
 
@@ -32,10 +38,30 @@
     [_imageView setCenter:self.view.center];
     _bakFrame = _imageView.frame;
     
-    UITapGestureRecognizer *tapGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(openView:)];
-    [self.view addGestureRecognizer:tapGesture];
+    [self.view addSubview:({
+        UIButton *button = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 100, 40)];
+        [button addTarget:self action:@selector(openView:) forControlEvents:UIControlEventTouchUpInside];
+        button.tag = AnimationTypeBasic;
+        [button setBackgroundColor:[UIColor orangeColor]];
+        [button setTitle:@"Basic" forState:UIControlStateNormal];
+        [button setCenter:CGPointMake(100, self.view.frame.size.height - 120)];
+        button;
+    })];
+    
+    [self.view addSubview:({
+        UIButton *button = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 100, 40)];
+        [button addTarget:self action:@selector(openView:) forControlEvents:UIControlEventTouchUpInside];
+        button.tag = AnimationTypeSpring;
+        [button setBackgroundColor:[UIColor purpleColor]];
+        [button setTitle:@"Spring" forState:UIControlStateNormal];
+        [button setCenter:CGPointMake(self.view.frame.size.width - 100, self.view.frame.size.height - 120)];
+
+        button;
+    })];
     
     _presenting = NO;
+    
+    [self.view bringSubviewToFront:_imageView];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -47,7 +73,11 @@
     [super viewWillAppear:animated];
 
     if (_presenting) {
-        [self closeAnimation];
+        if (_animationType == AnimationTypeBasic) {
+            [self closeAnimationBasic];
+        } else {
+            [self closeAnimationSpring];
+        }
         _presenting = NO;
     }
 }
@@ -55,40 +85,75 @@
 - (void)openView:(id)sender {
     if ( !_presenting) {
         _presenting = YES;
-        [self openAnimation];
+        _animationType = [(UIButton *)sender tag];
+        if (_animationType == AnimationTypeBasic) {
+            [self openAnimationBasic];
+        } else {
+            [self openAnimationSpring];
+        }
 
         _imageViewController = [[FullscreenImageViewController alloc] init];
         _imageViewController.transitionImageView = _imageView;
     }
 }
 
-- (POPBasicAnimation *)basicAnimationToValue:(id)value duration:(CGFloat)duration propertyName:(NSString *)name {
+- (POPBasicAnimation *)basicAnimationToValue:(id)value propertyName:(NSString *)name {
     POPBasicAnimation *animation = [POPBasicAnimation animationWithPropertyNamed:name];
     animation.toValue = value;
-    animation.duration = duration;
+    animation.duration = 0.5f;
     animation.removedOnCompletion = YES;
     
     return animation;
 }
 
-- (void)openAnimation {
-    [_imageView pop_addAnimation:[self basicAnimationToValue:[NSValue valueWithCGPoint:self.view.center] duration:0.5 propertyName:kPOPViewCenter] forKey:@"positionAnimation"];
+- (POPSpringAnimation *)springAnimationToValue:(id)value propertyName:(NSString *)name {
+    POPSpringAnimation *animation = [POPSpringAnimation animationWithPropertyNamed:name];
+    animation.toValue = value;
+    animation.springBounciness = 8;
+    animation.springSpeed = 4;
+    animation.removedOnCompletion = YES;
     
-    [_imageView pop_addAnimation:[self basicAnimationToValue:[NSValue valueWithCGRect:CGRectMake(0, 0, self.view.bounds.size.height, self.view.bounds.size.width)] duration:0.5 propertyName:kPOPViewBounds] forKey:@"boundsAnimation"];
+    return animation;
+}
 
-    [_imageView.layer pop_addAnimation:[self basicAnimationToValue:@(M_PI_2) duration:0.5 propertyName:kPOPLayerRotation] forKey:@"transformAnimation"];
+- (void)openAnimationBasic {
+    [_imageView pop_addAnimation:[self basicAnimationToValue:[NSValue valueWithCGPoint:self.view.center] propertyName:kPOPViewCenter] forKey:@"positionAnimation"];
+    
+    [_imageView pop_addAnimation:[self basicAnimationToValue:[NSValue valueWithCGRect:CGRectMake(0, 0, self.view.bounds.size.height, self.view.bounds.size.width)] propertyName:kPOPViewBounds] forKey:@"boundsAnimation"];
+
+    [_imageView.layer pop_addAnimation:[self basicAnimationToValue:@(M_PI_2) propertyName:kPOPLayerRotation] forKey:@"transformAnimation"];
     
     [[_imageView.layer pop_animationForKey:@"transformAnimation"] setCompletionBlock:^{
         [self presentViewController:_imageViewController animated:NO completion:nil];
     }];
 }
 
-- (void)closeAnimation {
-    [_imageView pop_addAnimation:[self basicAnimationToValue:[NSValue valueWithCGPoint:CGPointMake(_bakFrame.origin.x + _bakFrame.size.width / 2, _bakFrame.origin.y + _bakFrame.size.height / 2)] duration:0.5 propertyName:kPOPViewCenter] forKey:@"positionAnimation"];
+- (void)openAnimationSpring {
+    [_imageView pop_addAnimation:[self springAnimationToValue:[NSValue valueWithCGPoint:self.view.center] propertyName:kPOPViewCenter] forKey:@"positionAnimation"];
     
-    [_imageView pop_addAnimation:[self basicAnimationToValue:[NSValue valueWithCGRect:_bakFrame] duration:0.5 propertyName:kPOPViewBounds] forKey:@"boundsAnimation"];
+    [_imageView pop_addAnimation:[self springAnimationToValue:[NSValue valueWithCGRect:CGRectMake(0, 0, self.view.bounds.size.height, self.view.bounds.size.width)] propertyName:kPOPViewBounds] forKey:@"boundsAnimation"];
     
-    [_imageView.layer pop_addAnimation:[self basicAnimationToValue:@(0) duration:0.5 propertyName:kPOPLayerRotation] forKey:@"transformAnimation"];
+    [_imageView.layer pop_addAnimation:[self springAnimationToValue:@(M_PI_2) propertyName:kPOPLayerRotation] forKey:@"transformAnimation"];
+    
+    [[_imageView.layer pop_animationForKey:@"transformAnimation"] setCompletionBlock:^{
+        [self presentViewController:_imageViewController animated:NO completion:nil];
+    }];
+}
+
+- (void)closeAnimationBasic {
+    [_imageView pop_addAnimation:[self basicAnimationToValue:[NSValue valueWithCGPoint:CGPointMake(_bakFrame.origin.x + _bakFrame.size.width / 2, _bakFrame.origin.y + _bakFrame.size.height / 2)] propertyName:kPOPViewCenter] forKey:@"positionAnimation"];
+    
+    [_imageView pop_addAnimation:[self basicAnimationToValue:[NSValue valueWithCGRect:_bakFrame] propertyName:kPOPViewBounds] forKey:@"boundsAnimation"];
+    
+    [_imageView.layer pop_addAnimation:[self basicAnimationToValue:@(0) propertyName:kPOPLayerRotation] forKey:@"transformAnimation"];
+}
+
+- (void)closeAnimationSpring {
+    [_imageView pop_addAnimation:[self springAnimationToValue:[NSValue valueWithCGPoint:CGPointMake(_bakFrame.origin.x + _bakFrame.size.width / 2, _bakFrame.origin.y + _bakFrame.size.height / 2)] propertyName:kPOPViewCenter] forKey:@"positionAnimation"];
+    
+    [_imageView pop_addAnimation:[self springAnimationToValue:[NSValue valueWithCGRect:_bakFrame] propertyName:kPOPViewBounds] forKey:@"boundsAnimation"];
+    
+    [_imageView.layer pop_addAnimation:[self springAnimationToValue:@(0) propertyName:kPOPLayerRotation] forKey:@"transformAnimation"];
 }
 
 - (NSUInteger)supportedInterfaceOrientations {
