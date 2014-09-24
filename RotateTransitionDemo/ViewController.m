6c9128e41d0fcc52754dbc33590a13bedb36e4fd
Looping <www.looping@gmail.com>
2014-09-24 11:32:15 +0800

    Transition from origin image view, fix a presenting flash issue.

--- RotateTransitionDemo/ViewController.m
+++ RotateTransitionDemo/ViewController.m
@@ -12,8 +12,9 @@
 @interface ViewController ()
 
 @property (nonatomic) UIImageView *imageView;
-@property (nonatomic) UIImageView *animationImageView;
 @property (nonatomic) FullscreenImageViewController *imageViewController;
+@property (nonatomic) CGRect bakFrame;
+@property (nonatomic) BOOL presenting;
 
 @end
 
@@ -23,13 +24,17 @@
     [super viewDidLoad];
 
     [self.view setBackgroundColor:[UIColor lightGrayColor]];
+
     _imageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 100, 100)];
     [_imageView setImage:[UIImage imageNamed:@"avatar"]];
     [self.view addSubview:_imageView];
     [_imageView setCenter:self.view.center];
+    _bakFrame = _imageView.frame;
     
     UITapGestureRecognizer *tapGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(openView:)];
     [self.view addGestureRecognizer:tapGesture];
+    
+    _presenting = NO;
 }
 
 - (void)didReceiveMemoryWarning {
@@ -40,15 +45,15 @@
 - (void)viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
 
-    if (_animationImageView && _animationImageView.superview) {
-        [_animationImageView setHidden:NO];
-
+    if (_presenting) {
         [self closeAnimation];
+        _presenting = NO;
     }
 }
 
 - (void)openView:(id)sender {
-    if ( !(_animationImageView && _animationImageView.superview)) {
+    if ( !_presenting) {
+        _presenting = YES;
         [self openAnimation];
 
         _imageViewController = [[FullscreenImageViewController alloc] init];
@@ -57,26 +62,19 @@
 }
 
 - (void)openAnimation {
-    _animationImageView = [[UIImageView alloc] initWithFrame:_imageView.frame];
-
-    _animationImageView.image = _imageView.image;
-    [_imageView setHidden:YES];
-    
-    [[[UIApplication sharedApplication] keyWindow] addSubview: _animationImageView];
-    
     CABasicAnimation *center = [CABasicAnimation animationWithKeyPath:@"position"];
-    center.fromValue = [NSValue valueWithCGPoint:_animationImageView.layer.position];
+    center.fromValue = [NSValue valueWithCGPoint:_imageView.layer.position];
     center.toValue = [NSValue valueWithCGPoint:self.view.center];
     
     CABasicAnimation *scale = [CABasicAnimation animationWithKeyPath:@"bounds"];
-    scale.fromValue = [NSValue valueWithCGRect:_animationImageView.layer.bounds];
+    scale.fromValue = [NSValue valueWithCGRect:_imageView.layer.bounds];
     
     CABasicAnimation *rotate = [CABasicAnimation animationWithKeyPath:@"transform"];
-    rotate.fromValue = [NSValue valueWithCATransform3D:_animationImageView.layer.transform];
-    
+    rotate.fromValue = [NSValue valueWithCATransform3D:_imageView.layer.transform];
+
     scale.toValue = [NSValue valueWithCGRect:CGRectMake(0, 0, self.view.bounds.size.height, self.view.bounds.size.width)];
     
-    rotate.toValue = [NSValue valueWithCATransform3D:CATransform3DRotate(_animationImageView.layer.transform, M_PI_2, 0, 0, 1)];
+    rotate.toValue = [NSValue valueWithCATransform3D:CATransform3DRotate(_imageView.layer.transform, M_PI_2, 0, 0, 1)];
     
     CAAnimationGroup *group = [CAAnimationGroup animation];
     group.duration = 0.5;
@@ -85,25 +83,25 @@
     group.animations = @[scale,rotate,center];
     [group setValue:@"open" forKey:@"animationType"];
     
-    _animationImageView.layer.position = [center.toValue CGPointValue];
-    _animationImageView.layer.bounds = [scale.toValue CGRectValue];
-    _animationImageView.layer.transform = [rotate.toValue CATransform3DValue];
-    [_animationImageView.layer addAnimation:group forKey:nil];
+    _imageView.layer.position = [center.toValue CGPointValue];
+    _imageView.layer.bounds = [scale.toValue CGRectValue];
+    _imageView.layer.transform = [rotate.toValue CATransform3DValue];
+    [_imageView.layer addAnimation:group forKey:nil];
 }
 
 - (void)closeAnimation {
     CABasicAnimation *center = [CABasicAnimation animationWithKeyPath:@"position"];
-    center.fromValue = [NSValue valueWithCGPoint:_animationImageView.layer.position];
-    center.toValue = [NSValue valueWithCGPoint:_imageView.center];
+    center.fromValue = [NSValue valueWithCGPoint:_imageView.layer.position];
+    center.toValue = [NSValue valueWithCGPoint:CGPointMake(_bakFrame.origin.x + _bakFrame.size.width / 2, _bakFrame.origin.y + _bakFrame.size.height / 2)];
     
     CABasicAnimation *scale = [CABasicAnimation animationWithKeyPath:@"bounds"];
-    scale.fromValue = [NSValue valueWithCGRect:_animationImageView.layer.bounds];
+    scale.fromValue = [NSValue valueWithCGRect:_imageView.layer.bounds];
     CABasicAnimation *rotate = [CABasicAnimation animationWithKeyPath:@"transform"];
-    rotate.fromValue = [NSValue valueWithCATransform3D:_animationImageView.layer.transform];
+    rotate.fromValue = [NSValue valueWithCATransform3D:_imageView.layer.transform];
     
-    scale.toValue = [NSValue valueWithCGRect:CGRectMake(0, 0, _imageView.frame.size.width, _imageView.frame.size.height)];
+    scale.toValue = [NSValue valueWithCGRect:_bakFrame];
     
-    rotate.toValue = [NSValue valueWithCATransform3D:CATransform3DRotate(_animationImageView.layer.transform, -M_PI_2, 0, 0, 1)];
+    rotate.toValue = [NSValue valueWithCATransform3D:CATransform3DRotate(_imageView.layer.transform, -M_PI_2, 0, 0, 1)];
     
     CAAnimationGroup *group = [CAAnimationGroup animation];
     group.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseOut];
@@ -112,19 +110,14 @@
     group.animations = @[scale,rotate,center];
     [group setValue:@"close" forKey:@"animationType"];
     
-    _animationImageView.layer.position = [center.toValue CGPointValue];
-    _animationImageView.layer.bounds = [scale.toValue CGRectValue];
-    _animationImageView.layer.transform = [rotate.toValue CATransform3DValue];
-    [_animationImageView.layer addAnimation:group forKey:nil];
+    _imageView.layer.position = [center.toValue CGPointValue];
+    _imageView.layer.bounds = [scale.toValue CGRectValue];
+    _imageView.layer.transform = [rotate.toValue CATransform3DValue];
+    [_imageView.layer addAnimation:group forKey:nil];
 }
 
 - (void)animationDidStop:(CAAnimation *)anim finished:(BOOL)flag {
-    if ([[anim valueForKey:@"animationType"] isEqual:@"close"]) {
-        [_animationImageView removeFromSuperview];
-        _animationImageView = nil;
-        [_imageView setHidden:NO];
-    } else if ([[anim valueForKey:@"animationType"] isEqual:@"open"]) {
-        [_animationImageView setHidden:YES];
+    if ([[anim valueForKey:@"animationType"] isEqual:@"open"]) {
         [self presentViewController:_imageViewController animated:NO completion:nil];
     }
 }
